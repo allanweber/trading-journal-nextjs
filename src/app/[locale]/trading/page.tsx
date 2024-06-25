e96241741cd10a4c9770452441c31552c53a7392
Allan <a.cassianoweber@gmail.com>
2024-06-25 19:45:25 +0200

    Charts

--- src/app/[locale]/trading/page.tsx
+++ src/app/[locale]/trading/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import {
   Card,
   CardContent,
@@ -12,12 +14,10 @@ import {
   ChevronRightIcon,
   CopyIcon,
   CreditCardIcon,
-  DollarSign,
   FileIcon,
   ListFilterIcon,
   MoveVerticalIcon,
   TruckIcon,
-  Users,
 } from 'lucide-react';
 
 import { Badge } from '@/components/ui/badge';
@@ -46,6 +46,42 @@ import {
   TableRow,
 } from '@/components/ui/table';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import { Bar, BarChart, Line, LineChart, ResponsiveContainer } from 'recharts';
+
+const data = [
+  {
+    revenue: 10400,
+    subscription: 240,
+  },
+  {
+    revenue: 14405,
+    subscription: 300,
+  },
+  {
+    revenue: 9400,
+    subscription: 200,
+  },
+  {
+    revenue: 8200,
+    subscription: 278,
+  },
+  {
+    revenue: 7000,
+    subscription: 189,
+  },
+  {
+    revenue: 9600,
+    subscription: 239,
+  },
+  {
+    revenue: 11244,
+    subscription: 278,
+  },
+  {
+    revenue: 26475,
+    subscription: 189,
+  },
+];
 
 export default function Page() {
   return (
@@ -81,31 +117,69 @@ export default function Page() {
         </Card>
         <Card>
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-sm font-medium">Total Revenue</CardTitle>
-            <DollarSign className="h-4 w-4 text-muted-foreground" />
+            <CardTitle className="text-base font-normal">
+              Total Revenue
+            </CardTitle>
           </CardHeader>
           <CardContent>
-            <div className="text-2xl font-bold">$45,231.89</div>
-          </CardContent>
-          <CardFooter>
+            <div className="text-2xl font-bold">$15,231.89</div>
             <p className="text-xs text-muted-foreground">
               +20.1% from last month
             </p>
-          </CardFooter>
+            <div className="h-[80px]">
+              <ResponsiveContainer width="100%" height="100%">
+                <LineChart
+                  data={data}
+                  margin={{
+                    top: 5,
+                    right: 10,
+                    left: 10,
+                    bottom: 0,
+                  }}
+                >
+                  <Line
+                    type="monotone"
+                    strokeWidth={2}
+                    dataKey="revenue"
+                    activeDot={{
+                      r: 6,
+                      style: { fill: 'hsl(var(--primary))', opacity: 0.25 },
+                    }}
+                    style={
+                      {
+                        stroke: 'hsl(var(--primary))',
+                      } as React.CSSProperties
+                    }
+                  />
+                </LineChart>
+              </ResponsiveContainer>
+            </div>
+          </CardContent>
         </Card>
         <Card>
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-sm font-medium">Subscriptions</CardTitle>
-            <Users className="h-4 w-4 text-muted-foreground" />
+            <CardTitle className="text-base font-normal">
+              Subscriptions
+            </CardTitle>
           </CardHeader>
           <CardContent>
             <div className="text-2xl font-bold">+2350</div>
-          </CardContent>
-          <CardFooter>
             <p className="text-xs text-muted-foreground">
               +180.1% from last month
             </p>
-          </CardFooter>
+            <div className="mt-4 h-[80px]">
+              <ResponsiveContainer width="100%" height="100%">
+                <BarChart data={data}>
+                  <Bar
+                    dataKey="subscription"
+                    style={{
+                      fill: 'hsl(var(--primary))',
+                    }}
+                  />
+                </BarChart>
+              </ResponsiveContainer>
+            </div>
+          </CardContent>
         </Card>
       </div>
       <div className="grid grid-cols-1 gap-4 lg:grid-cols-3">
